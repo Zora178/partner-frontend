d1b9aeb05e5accb9d94b5e2c19f63ba52750d8c1
zora <w15363012507@163.com>
2024-06-11 19:57:06 +0800

    新增请求axios

---
+++ src/plugins/myAxios.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+
+const myAxios = axios.create({
+    baseURL: 'http://localhost:8080/api'
+  });
+
+// Add a request interceptor
+myAxios.interceptors.request.use(function (config) {
+    // Do something before request is sent
+    console.log('要发送请求啦',config)
+    return config;
+  }, function (error) {
+    // Do something with request error
+    return Promise.reject(error);
+  });
+
+// Add a response interceptor
+myAxios.interceptors.response.use(function (response) {
+    // Do something with response data
+    console.log('收到响应了' ,response)
+    return response;
+  }, function (error) {
+    // Do something with response error
+    return Promise.reject(error);
+  });
+
+
+export default myAxios;
